3643e7456db45da1927f8fd3b9fc5a555804f804
Janum Trivedi <jtrivedi@me.com>
2022-04-11 16:16:47 -0700

    Initial Commit

---
+++ WalletAnimation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WalletAnimation
+//
+//  Created by Janum Trivedi on 4/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
